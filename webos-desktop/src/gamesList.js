ad13bc76a7830e8b2d4841f8d00133448ca00bae
TheLp281 <efekantunc0@gmail.com>
2026-05-18 01:26:55 +0200

    Add angry2

--- webos-desktop/src/gamesList.js
+++ webos-desktop/src/gamesList.js
@@ -1,4 +1,10 @@
 export const appMap = {
+  angryBirds2: {
+    type: "game",
+    url: "/static/games/html/angryBirds2.html",
+    icon: "/static/icons/angryBirds2.webp",
+    title: "Angry Birds 2"
+  },
   slimeRancher: {
     type: "game",
     url: "https://raw.githack.com/Reeyuki/SlimeRanch/refs/heads/main/index.html",
