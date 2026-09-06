5ca78c9c13cd504297297fdebd37ecee3164e661
TheLp281 <efekantunc0@gmail.com>
2026-09-07 01:42:09 +0200

    add wisp server

--- webos-desktop/src/shared/wispConfig.js
+++ webos-desktop/src/shared/wispConfig.js
@@ -1,10 +1,11 @@
 import { os, StorageKeys } from "../framework.js";
 
-export const DEFAULT_WISP_URL = "wss://hurt-agata-liventcord-api-7072e9a6.koyeb.app/";
+export const DEFAULT_WISP_URL = "wss://probuildingsupplies.com/w/";
 
 export const WISP_SERVERS = [
-  { name: "Reeyuki Wisp", url: DEFAULT_WISP_URL },
+  { name: "Probuilding Wisp", url: DEFAULT_WISP_URL },
   { name: "Mercury Wisp", url: "wss://wisp.mercurywork.shop/" },
+  { name: "Reeyuki Wisp", url: "wss://hurt-agata-liventcord-api-7072e9a6.koyeb.app/" },
   { name: "Reeyuki Wisp 2", url: "wss://reeyukiwisp.onrender.com/" }
 ];
 
